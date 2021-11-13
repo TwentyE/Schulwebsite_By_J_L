e09bfd21586580a84b6cc2aa28f8b942b55bdc4d
TwentyE <62611484+TwentyE@users.noreply.github.com>
2021-11-13 16:36:53 +0100

    Updated formatting for index.php

--- School_Website/index.php
+++ School_Website/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+  
   <?php include("meta.php") ?>
   <body>
     <?php include("header.php") ?>
@@ -9,23 +10,26 @@
   <a href="#">Willkommen auf unserer Schulwebseite!</a>
   <p>Melde dich mit deinen Zugangsdaten an um auf unseren eigenen Vertretungsplan zuzugreifen. </p>
 </div>
+    
 <div class="article">
   <img src="Bilder/vorschau.png" alt="pepe">
   <a href="">Das ist der Titel von diesem Beitrag</a>
   <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
     sed diam voluptua. </p>
 </div>
+    
 <div class="article">
   <img src="Bilder/vorschau.png" alt="Vorschau">
   <a href="#">Das ist der Titel von diesem Beitrag</a>
   <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
     sed diam voluptua. </p>
 </div>
+    
   </section>
   <?php include("sidebar.php") ?>
   <?php include("footer.php") ?>
 </div>
 
-
   </body>
+
 </html>
